1d885a51408e09485a4aaa3eb54eebac7c18b9f0
pbarzaghi <pablobarzaghi@gmail.com>
2024-04-28 21:44:28 -0300

    para que pueda ejecutar la app con get

--- src/main/java/com/gruporoble/condiciones/GruporobleApplication.java
+++ src/main/java/com/gruporoble/condiciones/GruporobleApplication.java
@@ -2,6 +2,10 @@ package com.gruporoble.condiciones;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class GruporobleApplication {
@@ -10,4 +14,17 @@ public class GruporobleApplication {
 		SpringApplication.run(GruporobleApplication.class, args);
 	}
 
+	@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
+	}
 }
